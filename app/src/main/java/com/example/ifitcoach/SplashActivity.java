d1554c1324b2e5c12f5ca835ad0e17aa3bf45073
markgee-ui <macgeengugi@gmail.com>
2024-08-01 18:41:51 +0300

    ifit telehealth application

---
+++ app/src/main/java/com/example/ifitcoach/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.ifitcoach;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                mainIntent.putExtra("intentFrom", "SplashActivity");
+                startActivity(mainIntent);
+            }
+        },1000);
+
+
+    }
+}
